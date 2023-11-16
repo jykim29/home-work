2fb17875798c64006311e889ee4a9e0e5c5d6de3
jykim29 <skt13181@naver.com>
2023-11-17 00:46:30 +0900

    mission-03 페이지 셀렉트박스 버그 수정

--- mission-03/mission-03.js
+++ mission-03/mission-03.js
@@ -1,45 +1,50 @@
-const selectBox = document.querySelector(".custom-selectbox.select-language");
-const optionList = document.querySelector(".option-list");
-const optionItems = document.querySelectorAll(".option-item");
+const selectBoxContainer = document.querySelectorAll(".custom-selectbox-container");
+// const selectBox = document.querySelector(".custom-selectbox.select-language");
+// const optionList = document.querySelector(".option-list");
+// const optionItems = document.querySelectorAll(".option-item");
 const emailInput = document.querySelector(".input");
 const emailRegex = new RegExp("^[0-9a-zA-Z]+@[0-9a-zA-Z]+\\.[a-z]{2,}$");
 
-const selectBoxClickHandler = () => {
-  if (optionList.classList.contains("is--active")) {
+selectBoxContainer.forEach((container) => {
+  const selectBox = container.querySelector(".custom-selectbox.select-language");
+  const optionList = container.querySelector(".option-list");
+  const optionItems = optionList.querySelectorAll(".option-item");
+  const selectBoxClickHandler = () => {
+    if (optionList.classList.contains("is--active")) {
+      optionList.classList.remove("is--active");
+    } else {
+      optionList.classList.add("is--active");
+    }
+  };
+  const optionItemClickHandler = (e) => {
+    const value_1 = optionItems[0].innerText;
+    const value_2 = optionItems[1].innerText;
+    if (e.target.parentElement.classList.contains("first-item")) {
+      optionItems[0].classList.add("is--select");
+      optionItems[1].classList.remove("is--select");
+      selectBox.innerText = value_1;
+    } else {
+      optionItems[0].classList.remove("is--select");
+      optionItems[1].classList.add("is--select");
+      selectBox.innerText = value_2;
+    }
     optionList.classList.remove("is--active");
-  } else {
-    optionList.classList.add("is--active");
-  }
-};
-const optionItemClickHandler = (e) => {
-  const Item_1_Value = optionItems[0].innerText;
-  const Item_2_Value = optionItems[1].innerText;
-  if (e.target.parentElement.id === "first-item") {
-    optionItems[0].classList.add("is--select");
-    optionItems[1].classList.remove("is--select");
-    selectBox.innerText = Item_1_Value;
-  } else {
-    optionItems[0].classList.remove("is--select");
-    optionItems[1].classList.add("is--select");
-    selectBox.innerText = Item_2_Value;
-  }
-  optionList.classList.remove("is--active");
-};
-
-selectBox.addEventListener("click", selectBoxClickHandler);
-optionItems.forEach((item) => {
-  item.querySelector("a").addEventListener("click", optionItemClickHandler);
+  };
+  selectBox.addEventListener("click", selectBoxClickHandler);
+  optionItems.forEach((item) => {
+    item.querySelector("a").addEventListener("click", optionItemClickHandler);
+  });
 });
 
-emailInput.addEventListener("focus", (e) => {
+const inputFocusHandler = (e) => {
   e.target.classList.add("is--focus");
-});
-emailInput.addEventListener("blur", (e) => {
+};
+const inputBlurHandler = (e) => {
   const { value } = e.target;
   if (value.length > 0) return;
   e.target.classList.remove("is--focus");
-});
-emailInput.addEventListener("input", (e) => {
+};
+const inputHandler = (e) => {
   const { value } = e.target;
   const isValid = emailRegex.test(value);
   if (value.length > 0) {
@@ -54,4 +59,7 @@ emailInput.addEventListener("input", (e) => {
     e.target.classList.remove("is--valid");
     e.target.classList.remove("is--invalid");
   }
-});
+};
+emailInput.addEventListener("focus", inputFocusHandler);
+emailInput.addEventListener("blur", inputBlurHandler);
+emailInput.addEventListener("input", inputHandler);
